7df9c16a441799790ebb7a92c73e789f209c0584
codenestlearning <146897863+codenestlearning@users.noreply.github.com>
2023-10-04 05:51:10 +0530

    Add files via upload

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person obj = new Person("mahi", 22);
+
+            Console.WriteLine(obj);
+            Console.ReadLine();
+        }
+    }
+}
